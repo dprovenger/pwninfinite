c77e70e6cb267faecfec05f5c90d5cc06a41f791
Jorge Arrieta <jorge.r.arrieta.jr@gmail.com>
2025-02-06 00:10:42 -0600

    Adding comments to scanner

--- standAloneModules/iaCSecurityScan.py
+++ standAloneModules/iaCSecurityScan.py
@@ -2,6 +2,7 @@ import subprocess
 import sys
 
 def iac_scan(file_path):
+  """Runs checkov to scan an IaC file for security issues."""
   try:
       result = subprocess.run(["checkov", "-f", file_path], capture_output=True, text=True)
       print(result.stdout)
@@ -11,6 +12,7 @@ def iac_scan(file_path):
       sys.exit(1)
 
 def main():
+    """Main function to handle user input and run the IaC scan"""
     if len(sys.argv) < 2:
         print("Usage: python3 iac_scanner.py <IaC file>")
         sys.exit(1)
